e34f3df6d092a3e22ec961420e8563229c2f2ac2
astr0bas3d <amplice@protonmail.com>
2020-12-23 12:06:43 +0800

    whoops broke it for 1 sec

--- src/Strat.js
+++ src/Strat.js
@@ -1,17 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import AWS from 'aws-sdk'
-import moonbased from './moonbased.mp4'
-import axios from 'axios'
 let authheader = 'Bearer ' + process.env.REACT_APP_LINODE_ACCESS_KEY
 
-let config = {
-    headers: {
-      'Authorization': authheader
-    }
-  }
-
-const baseurl = 'https://api.linode.com/v4/object-storage/buckets/us-east-1/basedtv/'
-  
 const Strat = () => {
 
     return (
